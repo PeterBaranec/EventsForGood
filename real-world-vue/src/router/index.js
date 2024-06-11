4dc5866b2f480829e469eb2983584f8b4387da54
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-06-11 16:40:21 +0200

    init

---
+++ real-world-vue/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '@/views/EventListView.vue'
+import EventsDetailsView from '@/views/EventsDetailsView.vue'
+import AboutView from '../views/AboutView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventListView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventsDetailsView
+    }
+  ]
+})
+
+export default router
